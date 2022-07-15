d5590c65100f37c83d22d7f2054770ffb9c0d660
himozzza <himozzza.propionat@gmail.com>
2022-07-15 23:49:12 +0300

    Окончательный релиз.

--- eldenring_KILL.sh
+++ eldenring_KILL.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+attempts=1
 stop_flag=true
-while $stop_flag == true;
+while [ $stop_flag = true ];
 do
     /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=/app/bin/steam-wrapper com.valvesoftware.Steam steam://rungameid/1245620 &
     sleep 30
-    pid_id=`ps aux | grep -i eldenring.exe | grep -v grep | awk '{print $2}'`
-    memory_kb=`cat /proc/$pid_id/status | grep -i vmhwm | awk '{print $2}' 2> /dev/null`
-    memory_mb=$(( $memory_kb / 1024 ))
-    if (( $memory_mb < 1000 ));
+    pid_id=$(pgrep -f eldenring.exe)
+    memory_kb=$(grep -i vmhwm /proc/"$pid_id"/status | awk '{print $2}' 2> /dev/null)
+    memory_mb=$(( memory_kb / 1024 ))
+    if (( memory_mb < 1000 ));
     then
-        kill -9 $pid_id 2> /dev/null
+        kill -9 "$pid_id" 2> /dev/null
         sleep 2
+        ((attempts++))
     else
-        wait $pid_id
+        echo -ne "$(date +%F%t%T)\nИгра зарущена с $attempts раза.\n\n" >> /home/"$USER"/EldenRing.log
         stop_flag=false
     fi
 done
+exit 0
